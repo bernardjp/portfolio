ffd9fcc1069b5b3afe2dc42c3edf1f9db70d4227
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2024-03-05 18:06:19 -0300

    Added an ID to each section of the page to allow navigation.

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,9 +1,9 @@
-import { Title, Text } from '@mantine/core';
+import { Title, Text, Container } from '@mantine/core';
 import classes from './AboutMe.module.css';
 
 export function AboutMe() {
   return (
-    <>
+    <Container id="about-me" size="lg">
       <Title className={classes.title} ta="center" mt={100}>
         About{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
@@ -13,6 +13,6 @@ export function AboutMe() {
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
         Personal Information
       </Text>
-    </>
+    </Container>
   );
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,9 @@
-import { Title, Text } from '@mantine/core';
+import { Title, Text, Container } from '@mantine/core';
 import classes from './Hero.module.css';
 
 export function Hero() {
   return (
-    <>
+    <Container id="hero" size="lg" className={classes.container}>
       <Title className={classes.title} ta="center" mt={100}>
         Juan Pablo{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
@@ -13,6 +13,6 @@ export function Hero() {
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
         Software Developer
       </Text>
-    </>
+    </Container>
   );
 }
