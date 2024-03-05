17c4b9163afb3497fd2010b5557a83859fbbaceb
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2024-03-05 17:05:42 -0300

    Added custom CSS Variables to Mantine's base theme.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
-import { theme } from './theme';
+import { theme, themeResolver } from './theme';
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider theme={theme} cssVariablesResolver={themeResolver}>
       <Router />
     </MantineProvider>
   );
