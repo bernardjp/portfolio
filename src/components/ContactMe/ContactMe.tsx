cf6e27e76517bd49464bf2aec4fae3c32c440c0e
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2024-03-05 18:06:19 -0300

    Added an ID to each section of the page to allow navigation.

--- src/components/ContactMe/ContactMe.tsx
+++ src/components/ContactMe/ContactMe.tsx
@@ -1,9 +1,9 @@
-import { Title, Text } from '@mantine/core';
+import { Container, Title, Text } from '@mantine/core';
 import classes from './ContactMe.module.css';
 
 export function ContactMe() {
   return (
-    <>
+    <Container id="contact-me" size="lg">
       <Title className={classes.title} ta="center" mt={100}>
         Contact{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
@@ -13,6 +13,6 @@ export function ContactMe() {
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
         Let&apos;s work together
       </Text>
-    </>
+    </Container>
   );
 }
--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,15 +1,15 @@
-import { Title, Text } from '@mantine/core';
+import { Title, Text, Container } from '@mantine/core';
 import classes from './Portfolio.module.css';
 
 export function Portfolio() {
   return (
-    <>
+    <Container id="projects" size="lg">
       <Title className={classes.title} ta="center" mt={100}>
         PORTFOLIO
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
         These are some of my projects!
       </Text>
-    </>
+    </Container>
   );
 }
