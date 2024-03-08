6e35466e8cdc81b2d68d3b5398c6dba5f228ce9e
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2024-03-08 16:33:23 -0300

    Adjusted the header's height and added a footer's height variable to the theme manager.

--- src/theme.ts
+++ src/theme.ts
@@ -2,8 +2,10 @@ import { CSSVariablesResolver, createTheme } from '@mantine/core';
 
 const theme = createTheme({
   other: {
-    headerHeight: '100px',
-    footerHeight: '100px',
+    headerHeight: '5rem',
+    footerHeight: '5rem',
+    headerHeightMobile: '3rem',
+    footerHeightMobile: '3rem',
   },
 });
 
@@ -11,6 +13,8 @@ const themeResolver: CSSVariablesResolver = (baseTheme) => ({
   variables: {
     '--header-height': baseTheme.other.headerHeight,
     '--footer-height': baseTheme.other.footerHeight,
+    '--header-height-mobile': baseTheme.other.headerHeightMobile,
+    '--footer-height-mobile': baseTheme.other.footerHeightMobile,
   },
   dark: {},
   light: {},
